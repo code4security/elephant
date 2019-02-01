82ad7eeef5ca677d37d44ad588c0695898ecc8b8
duhui <d1769111741@163.com>
2019-02-01 10:12:32 +0800

    2.1 添加功能验证玩家角色名，并重新排版

--- elephant-client/src/main/java/com/sjhy/platform/client/dto/config/ProtocolKey.java
+++
@@ -1,8 +0,0 @@
-package com.sjhy.platform.client.dto.config;
-/**
- * @HJ
- */
-public class ProtocolKey {
-	public static final String UNIQUE_DEVICE_ID_KEY = "haifang_z68_heross&^86";
-	public static final String ACCOUNT_PASSWORD_KEY = "haifang_z66_account#@88";
-}
